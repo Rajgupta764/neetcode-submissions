e9be2be0665830b48489fce4deab3a57297a924d
Raj kumar <raj.23jics087@jietjodhpur.ac.in>
2026-06-23 22:22:19 +0530

    Add: is-palindrome - submission-1

---
+++ Data Structures & Algorithms/is-palindrome/submission-1.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        int left=0;
+        String convert=s.toLowerCase().replaceAll("[^A-Za-z0-9]","");
+        int right=convert.length()-1;
+        while(left<=right){
+             if(convert.charAt(left)!=convert.charAt(right)){
+                return false;
+             }
+             left++;
+             right--;
+        }
+        return true;
+    }
+}
